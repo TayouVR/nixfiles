707263239eb95bf965be0e728c0fbf30d3b4ffb5
Tayou <git@tayou.org>
2025-04-15 02:13:45 +0200

    add kernel patch for cap_sys_nice

--- configs/common/optional/graphics/amd.nix
+++ configs/common/optional/graphics/amd.nix
@@ -1,10 +1,18 @@
-{...}:
+{ pkgs, ... }:
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelPatches = [
     {
       name = "bigscreen beyond";
       patch = ./beyondKernel.patch;
+    }
+    { # see https://wiki.nixos.org/wiki/VR#Applying_as_a_NixOS_kernel_patch
+      name = "amdgpu-ignore-ctx-privileges";
+      patch = pkgs.fetchpatch {
+        name = "cap_sys_nice_begone.patch";
+        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+      };
     }
   ];
 
