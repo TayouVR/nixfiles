604da43a497ea37745b61ac685ab1bab5ec58fdd
Tayou <git@tayou.org>
2025-05-23 20:41:20 +0200

    install a few instant messenger clients

--- configs/common/required/communication.nix
+++ configs/common/required/communication.nix
@@ -4,6 +4,13 @@
 }:
 {
   config = {
+    # libolm is insecure, but is needed as dep for many matrix clients.
+    # remove this exception and look at build log to see more details
+    # this exception didn't work... I don't need neochat that much I guess lol
+#    nixpkgs.config.permittedInsecurePackages = [
+#      "olm-3.2.16"
+#    ];
+
     # Communication and social media packages
     environment.systemPackages = with pkgs; [
       # Web browsers
@@ -18,6 +25,10 @@
       discord
       vesktop
       telegram-desktop
+      jami-client-qt
+      cinny-desktop
+      #kdePackages.neochat
+      element-desktop
 
       # File sharing
       localsend
