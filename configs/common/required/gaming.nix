e31ecb3247fb0148f341a38fb46c0069b9aa2759
Tayou <git@tayou.org>
2025-07-03 18:55:58 +0200

    disable global steam game launch args
    I should look into that thing someone posted for launch args for individual games

--- configs/common/required/gaming.nix
+++ configs/common/required/gaming.nix
@@ -11,14 +11,14 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
 
-      package = pkgs.steam.override {
-        extraProfile = ''
-          export OXR_VIEWPORT_SCALE_PERCENTAGE=70
-          export PRESSURE_VESSEL_FILESYSTEMS_RW="$XDG_RUNTIME_DIR/monado_comp_ipc"
-          export XRIZER_CUSTOM_BINDINGS_DIR="/mnt/linuxApps/SteamLibrary/steamapps/common/VRChat/OpenComposite/"
-          unset TZ
-        '';
-      };
+#      package = pkgs.steam.override {
+#        extraProfile = ''
+#          export OXR_VIEWPORT_SCALE_PERCENTAGE=70
+#          export PRESSURE_VESSEL_FILESYSTEMS_RW="$XDG_RUNTIME_DIR/monado_comp_ipc"
+#          export XRIZER_CUSTOM_BINDINGS_DIR="/mnt/linuxApps/SteamLibrary/steamapps/common/VRChat/OpenComposite/"
+#          unset TZ
+#        '';
+#      };
     };
 
     # Enable Waydroid for Android app support
