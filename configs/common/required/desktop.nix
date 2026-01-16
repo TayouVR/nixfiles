56663450e4da4b433ea15f6ba18df6e6263cf80c
Tayou <git@tayou.org>
2026-01-16 16:56:58 +0100

    enable obs virtual camera

--- configs/common/required/desktop.nix
+++ configs/common/required/desktop.nix
@@ -108,6 +108,7 @@
 
     programs.obs-studio = {
       enable = true;
+      enableVirtualCamera = true;
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
         obs-backgroundremoval
