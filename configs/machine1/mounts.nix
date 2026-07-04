c5d6351df27dbc9de8019cec4110625ff6267177
Tayou <git@tayou.org>
2026-07-04 17:55:39 +0200

    re-enable mounts

--- configs/machine1/mounts.nix
+++ configs/machine1/mounts.nix
@@ -11,31 +11,31 @@
     fsType = "ntfs";
     options = [ "uid=1000" "gid=100" "nofail" ];
   };
-#  fileSystems."/mnt/win10" = {
-#    device = "/dev/disk/by-uuid/4660E7FF60E7F419";
-#    fsType = "ntfs";
-#    options = [ "uid=1000" "gid=100" "nofail" ];
-#  };
-#  fileSystems."/mnt/win10_old" = {
-#    device = "/dev/disk/by-uuid/CE46DCF546DCDEF1";
-#    fsType = "ntfs";
-#    options = [ "uid=1000" "gid=100" "nofail" ];
-#  };
-#  fileSystems."/mnt/win7" = {
-#    device = "/dev/disk/by-uuid/19BBE1D72C7D6D07";
-#    fsType = "ntfs";
-#    options = [ "uid=1000" "gid=100" "nofail" ];
-#  };
+  fileSystems."/mnt/win10" = {
+    device = "/dev/disk/by-uuid/4660E7FF60E7F419";
+    fsType = "ntfs";
+    options = [ "uid=1000" "gid=100" "nofail" ];
+  };
+  fileSystems."/mnt/win10_old" = {
+    device = "/dev/disk/by-uuid/CE46DCF546DCDEF1";
+    fsType = "ntfs";
+    options = [ "uid=1000" "gid=100" "nofail" ];
+  };
+  fileSystems."/mnt/win7" = {
+    device = "/dev/disk/by-uuid/19BBE1D72C7D6D07";
+    fsType = "ntfs";
+    options = [ "uid=1000" "gid=100" "nofail" ];
+  };
   fileSystems."/mnt/linuxApps" = {
     device = "/dev/disk/by-uuid/35e4a507-0c0b-4524-86fb-796ea9684aab";
     fsType = "ext4";
     options = [ "nofail" ];
   };
-#  fileSystems."/mnt/btrfs" = {
-#    device = "/dev/disk/by-uuid/62b8795c-2ef9-4976-1034-60d00a275f8f";
-#    fsType = "btrfs";
-#    options = [ "nofail" ];
-#  };
+  fileSystems."/mnt/btrfs" = {
+    device = "/dev/disk/by-uuid/62b8795c-2ef9-4976-1034-60d00a275f8f";
+    fsType = "btrfs";
+    options = [ "nofail" ];
+  };
   fileSystems."/mnt/BACKUP" = {
     device = "/dev/disk/by-uuid/78525CB8525C7CB6";
     fsType = "ntfs";
@@ -78,5 +78,13 @@
     fsType = "none";
     options = [ "bind" "uid=1000" "gid=100" "nofail" ];
   };
+
+  # NAS
+#  fileSystems."/mnt/NAS/Data" = {
+#    device = "192.168.178.64:/run/media/MainPool/Data";
+#    fsType = "nfs";
+#    options = [ "x-systemd.automount" "noauto" "vers=3" "sec=sys" ];
+#  };
+
 
 }
